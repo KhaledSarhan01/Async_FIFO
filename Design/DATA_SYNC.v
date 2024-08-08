28613dddb083e29e0c6b3bd65ab63ee328b7bb7d
KhaledSarhan01 <khaledsarhaan@gmail.com>
2024-08-09 00:16:32 +0300

    FIFO: Syntax Errors are Fixed | Data_SYNC: Double Flipflop Synchronizer with Gray Encoding is Created

--- Design/DATA_SYNC.v
+++ Design/DATA_SYNC.v
@@ -1,7 +1,25 @@
 module DATA_SYNC #(parameter NUM_STAGES = 2 ,parameter BUS_WIDTH = 8)(
     input wire CLK,RST,
     input wire [BUS_WIDTH-1:0] Unsync_bus,
-    output wire [BUS_WIDTH-1:0] sync_bus,
+    output wire [BUS_WIDTH-1:0] sync_bus
 );
-  
+
+// Using Flip Flop synchronizer with Gray encoding input/output 
+    reg [BUS_WIDTH-1:0] synchronizer [NUM_STAGES-1:0] ;
+    integer i;
+
+    always @(posedge CLK or negedge RST ) begin
+        if (!RST) begin
+            for (i = 0; i <= NUM_STAGES-1; i=i+1 ) begin
+               synchronizer[i] <= 'b0; 
+            end 
+        end else begin
+            synchronizer[0] <= Unsync_bus;
+            for (i = 1; i<= NUM_STAGES-1; i=i+1) begin
+                synchronizer[i] <= synchronizer[i-1];
+           end
+        end
+    end
+
+    assign sync_bus = synchronizer[NUM_STAGES-1];
 endmodule
