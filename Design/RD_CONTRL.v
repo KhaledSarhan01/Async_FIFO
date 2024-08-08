d1f729c063a34c254fbb9bf7d32280c63fbb8dc9
KhaledSarhan01 <khaledsarhaan@gmail.com>
2024-08-09 01:17:03 +0300

    RD_CONTRL: rptr_empty is completed + add to the flow + syntax error free

--- Design/RD_CONTRL.v
+++ Design/RD_CONTRL.v
@@ -5,11 +5,51 @@ module RD_CONTRL #(parameter ADDR_WIDTH = 4)(
     output wire rempty,
     //pointers 
     input wire  [ADDR_WIDTH:0] w_ptr,
-    input wire w_ptr_en,
     output wire [ADDR_WIDTH:0] r_ptr,
-    output wire r_ptr_en,
     //memory
     output wire [ADDR_WIDTH-1:0] raddr
 );
+
+    //Registers
+    reg [ADDR_WIDTH:0] gray_ptr,bn_ptr;
+    reg empty_flag;
+
+    // Binary Encoded Pointer Logic
+    always @(posedge r_clk or negedge r_rst) begin
+        if (!r_rst) begin
+            bn_ptr <= 'b0;
+        end else begin
+            if(rinc & ~(empty_flag))begin
+                bn_ptr <= bn_ptr + 'd1;
+            end
+        end
+    end
+
+    // Gray Encoded Pointer Logic
+    integer i; 
+    always @(*) begin
+        for (i=0; i<ADDR_WIDTH; i=i+1) begin
+            gray_ptr[i] = bn_ptr[i] ^ bn_ptr[i+1];
+        end
+    end
+    
+    // Full Flag Logic
+    wire empty_condition;
+    assign empty_condition = (gray_ptr == w_ptr);
+    always @(posedge r_clk or negedge r_rst) begin
+        if (!r_rst) begin
+            empty_flag <= 1'b0;
+        end else begin
+            if (empty_condition) begin
+                empty_flag <= 1'b1;
+            end else begin
+                empty_flag <= 1'b0;
+            end
+        end
+    end
+    //Outputs
+    assign raddr = bn_ptr[ADDR_WIDTH-1:0];
+    assign r_ptr = gray_ptr;
+    assign rempty = empty_flag;
 
 endmodule
