b315136c99e7f7b0a594a0dad28bdf9c34bb1d06
KhaledSarhan01 <khaledsarhaan@gmail.com>
2024-08-12 00:19:39 +0300

    RD_CONTRL: make gray pointer combinational + remove condition on binary pointer

--- Design/RD_CONTRL.v
+++ Design/RD_CONTRL.v
@@ -19,15 +19,13 @@ module RD_CONTRL #(parameter ADDR_WIDTH = 4)(
         if (!r_rst) begin
             bn_ptr <= 'b0;
         end else begin
-            if(rinc & ~(empty_flag))begin
-                bn_ptr <= bn_ptr + 'd1;
-            end
+            bn_ptr <= bn_ptr + (rinc & ~(empty_flag));
         end
     end
 
     // Gray Encoded Pointer Logic
     integer i; 
-    always @(posedge r_clk or negedge r_rst) begin
+    always @(*) begin
         if (!r_rst) begin
             gray_ptr <= 'b0;
         end else begin
